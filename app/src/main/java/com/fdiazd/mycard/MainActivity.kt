a5dd2deb994a2fdcfee271a13e48988788c3ab2c
Fernando Díaz <fernando.diaz@nanodev.es>
2022-05-27 20:01:42 +0200

    Added Portfolio Card

--- app/src/main/java/com/fdiazd/mycard/MainActivity.kt
+++ app/src/main/java/com/fdiazd/mycard/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.fdiazd.mycard.ui.theme.MyCardTheme
@@ -41,6 +47,9 @@ fun Greeting(name: String) {
 
 @Composable
 fun CreateCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(value = false)
+    }
     Surface(modifier= Modifier
         .fillMaxWidth()
         .fillMaxHeight()) {
@@ -58,9 +67,18 @@ fun CreateCard() {
                 CreateImageProfile()
                 Divider()
                 CreateInfo()
-                Button(onClick = { /*TODO*/ }) {
+                Button(onClick = {
+                    buttonClickedState.value = !buttonClickedState.value
+                }
+                ) {
                     Text(text = "Portfolio",
                     style = MaterialTheme.typography.button)
+                }
+                if (buttonClickedState.value){
+                    Content()
+                }
+                else{
+                    Box{}
                 }
             }
         }
@@ -74,14 +92,15 @@ fun Content(){
         .fillMaxHeight()
         .fillMaxWidth()
         .padding(5.dp)) {
-        Surface(modifier = Modifier.padding(3.dp)
+        Surface(modifier = Modifier
+            .padding(3.dp)
             .fillMaxWidth()
             .fillMaxHeight(),
                 shape = RoundedCornerShape(corner = CornerSize(6.dp)),
                 border = BorderStroke(width = 2.dp,
                     color = Color.LightGray)) {
 
-                Portfolio(data = listOf("Project 1, Project 2, Project 3"))
+                Portfolio(data = listOf())
         }
     }
     
@@ -89,7 +108,61 @@ fun Content(){
 
 @Composable
 fun Portfolio(data: List<String>) {
+    LazyColumn {
+        // Add a single item
+        item {
+            CreateItemPortfolio()
+        }
 
+        item {
+            CreateItemPortfolio()
+        }
+
+        item {
+            CreateItemPortfolio()
+        }
+
+    }
+}
+
+@Composable
+private fun CreateItemPortfolio() {
+    Card(modifier = Modifier
+        .padding(13.dp)
+        .fillMaxWidth(),
+        shape = RectangleShape,
+        elevation = 4.dp) {
+        Row(modifier = Modifier
+            .padding(8.dp)
+            .background(MaterialTheme.colors.surface)
+            .padding(7.dp)) {
+
+            Surface(modifier = Modifier
+                .size(90.dp)
+                .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                elevation = 4.dp,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)) {
+
+                Image(painter = painterResource(id = R.drawable.avatar),
+                    contentDescription = "profile image",
+                    modifier = Modifier.size(95.dp),
+                    contentScale = ContentScale.Crop)
+
+            }
+            Column(modifier = Modifier
+                .padding(7.dp)
+                .align(alignment = Alignment.CenterVertically)) {
+
+                Text(text = "Developer Info App",
+                    fontWeight = FontWeight.Bold)
+                Text(text = "Shows info and portfolio's developer",
+                    style = MaterialTheme.typography.body2)
+            }
+
+        }
+    }
 }
 
 @Composable
